6ccf2488747b5e75ba733332d4088cda8422bc4d
Samuel <samuelchigo55@gmail.com>
2024-11-09 10:52:27 +0100

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const express = require("express")
+const logger = require('./middlewares/logger')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(logger)
+
+app.get("/", (req, res)=> {
+    res.send("Hello world")
+})
+
+
+
+app.listen(5000, () => {
+    console.log("Listening at port 5000")
+})
