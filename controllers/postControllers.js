0cada68f35c387ec8fbd78c015bcf04a8d0c8bc5
unknown <michaelcj284@gmail.com>
2024-11-09 12:07:37 +0100

    added post 4

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -11,7 +11,12 @@ const post = [
         id: 2,
         title: 'Post 2',
         content: 'This is the second post'
-    }
+    },
+    {
+        id: 4,
+        title: 'Post 4',
+        content: 'This is the fourth port'
+    },
 ]
 
 const createPost = async (req, res) => {
